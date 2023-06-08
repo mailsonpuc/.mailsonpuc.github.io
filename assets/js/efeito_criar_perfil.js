e6cb8e18ea658941a4416d7f107bba4bbf891f87
mailsonpuc <133193122+mailsonpuc@users.noreply.github.com>
2023-06-08 13:04:08 -0300

    Create efeito_criar_perfil.js
    efeito carregando

---
+++ assets/js/efeito_criar_perfil.js
@@ -0,0 +1,24 @@
+perfil = () => {
+    let timerInterval
+Swal.fire({
+  title: 'por favor espere..!',
+  html: 'Criando Perfil <b></b> segundos.',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+}
